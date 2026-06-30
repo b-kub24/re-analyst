69f11b4ab98bad84d7bc6e384bf450349d512d58
b-kub24 <131963818+b-kub24@users.noreply.github.com>
2026-06-30 17:32:31 -0400

    Fix next.config.js: webpack alias to resolve next/headers on client

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@supabase/ssr'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'next/headers': false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
